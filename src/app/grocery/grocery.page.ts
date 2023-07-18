c92301dff3e3058c78609e715bdaf2c6a1e84b2e
Adam Ure <adamqure@icloud.com>
2023-07-17 20:11:25 -0600

    Complete Week 4 CRUD Operations

--- src/app/grocery/grocery.page.ts
+++ src/app/grocery/grocery.page.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Grocery } from './grocery';
-import { IonItem, ToastController } from '@ionic/angular';
+import { ToastController } from '@ionic/angular';
 import { AlertController } from '@ionic/angular';
-import { BehaviorSubject } from 'rxjs';
 import { IonItemSliding } from '@ionic/angular';
-import { Directive, OnInit, EventEmitter, Output } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { GroceriesService } from '../services/groceries.service';
+import { InputDialogService } from '../services/input-dialog.service';
 
 @Component({
   selector: 'app-grocery',
@@ -12,77 +13,23 @@ import { Directive, OnInit, EventEmitter, Output } from '@angular/core';
   styleUrls: ['grocery.page.scss']
 })
 export class GroceryPage {
-  groceries: BehaviorSubject<Array<Grocery>> = new BehaviorSubject(new Array<Grocery>())
-  constructor(public toastCtrl: ToastController, public alertCtrl: AlertController) {}
+  groceries: Observable<Grocery[]> = new Observable()
 
-  ngOnInit() {
-    this.setDefaultGroceryList();
+  constructor(public dataService: GroceriesService, public inputDialogService: InputDialogService) {
+    this.groceries = this.dataService.getItems().asObservable();
   }
 
-  setDefaultGroceryList() {
-    this.groceries.next([
-      new Grocery("Banana", 12),
-      new Grocery("Pineapple", 1),
-      new Grocery("Orange", 6),
-      new Grocery("Box of Kiwis", 2),
-      new Grocery("Box of Strawberries", 3)
-    ])
+  async editItem(grocery: Grocery, index: number, listInput: IonItemSliding) {
+    let newGrocery = await this.inputDialogService.showItemPrompt('Edit Item', 'Please enter item...', grocery)
+    this.dataService.editItem(newGrocery, index, listInput);
   }
 
-  async removeItem(grocery: Grocery, index: number) {
-    console.log("Removing Item - ", grocery);
-    var groceryList = this.groceries.getValue();
-    groceryList.splice(index, 1);
-    this.groceries.next(groceryList);
-    const toast = await this.toastCtrl.create({
-      message: 'Removing Item - ' + grocery.name + " ...",
-      duration: 3000
-    });
-    await toast.present();
+  async removeItem(grocery: Grocery, index: number, listInput: IonItemSliding) {
+    this.dataService.removeItem(grocery, index, listInput);
   }
 
-  addItem() {
-    console.log("Adding Item");
-    this.showAddItemPrompt();
-  }
-
-  close(listInput: IonItemSliding) {
-    console.log("Closing item...")
-    listInput.close();
-  }
-
-  async showAddItemPrompt() {
-    const prompt = await this.alertCtrl.create({
-      header: 'Add Item',
-      message: "Please enter item...",
-      inputs: [
-        {
-          name: 'name',
-          placeholder: 'Name'
-        },
-        {
-          name: 'count',
-          placeholder: 'Count'
-        },
-      ],
-      buttons: [
-        {
-          text: 'Cancel',
-          handler: data => {
-            console.log('Cancel clicked');
-          }
-        },
-        {
-          text: 'Save',
-          handler: item => {
-            console.log('Saved clicked', item);
-            var groceryList = this.groceries.getValue();
-            groceryList.push(new Grocery(item.name, item.count));
-            this.groceries.next(groceryList);
-          }
-        }
-      ]
-    });
-    await prompt.present();
+  async addItem() {
+    let newGrocery = await this.inputDialogService.showItemPrompt('Add Item', 'Please enter item...');
+    this.dataService.addItem(newGrocery);
   }
 }
